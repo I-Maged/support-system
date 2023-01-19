40b9f5600cd9f55cb9313b5931abc29a4aa5b37b
I-Maged <magedhanafy@gmail.com>
2023-01-19 10:10:19 +0200

    forms

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa';
+
 const Home = () => {
   return (
     <>
@@ -5,6 +8,12 @@ const Home = () => {
         <h1>What do you need help with?</h1>
         <p>Please choose from an option below</p>
       </section>
+      <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+        <FaQuestionCircle /> Create New Ticket
+      </Link>
+      <Link to='/tickets' className='btn btn-block'>
+        <FaTicketAlt /> View My Tickets
+      </Link>
     </>
   );
 };
