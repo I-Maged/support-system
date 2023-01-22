f19d37b996c2a69ef0082134ad5607ce68af2fea
I-Maged <magedhanafy@gmail.com>
2023-01-22 11:07:08 +0200

    update & delete functionality

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -19,6 +19,32 @@ const getTickets = asyncHandler(async (req, res) => {
   res.status(200).json(tickets);
 });
 
+// @desc    Get a single ticket
+// @route   GET /api/tickets/:id
+// @access  Private
+const getTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  res.status(200).json(ticket);
+});
+
 // @desc    create new ticket
 // @route   POST /api/tickets
 // @access  Private
@@ -47,4 +73,68 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
-module.exports = { getTickets, createTicket };
+// @desc    Delete a single ticket
+// @route   DELETE /api/tickets/:id
+// @access  Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  await ticket.remove();
+
+  res.status(200).json({ success: true });
+});
+
+// @desc    update ticket
+// @route   PUT /api/tickets/:id
+// @access  Private
+const updateTicket = asyncHandler(async (req, res) => {
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error('Please include a product and description');
+  }
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('You must login to create a ticket');
+  }
+
+  const ticket = await Ticket.create({
+    user: req.user.id,
+    product,
+    description,
+    status: 'new',
+  });
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body);
+
+  res.status(201).json(updatedTicket);
+});
+
+module.exports = {
+  getTickets,
+  createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
+};
