4a2f326bb4bad9212af62b2a17c64f2e9492b715
I-Maged <magedhanafy@gmail.com>
2023-01-22 06:02:53 +0200

    get & create tickets functionalty

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -7,26 +7,44 @@ const Ticket = require('../models/ticketModel');
 // @route   GET /api/tickets
 // @access  Private
 const getTickets = asyncHandler(async (req, res) => {
-  //   const tickets = {
-  //     id: req.user._id,
-  //     email: req.user.email,
-  //     name: req.user.name,
-  //   };
-  //   res.status(200).json(tickets);
-  res.status(200).json({ message: 'get tickets' });
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+
+  res.status(200).json(tickets);
 });
 
 // @desc    create new ticket
 // @route   POST /api/tickets
 // @access  Private
 const createTicket = asyncHandler(async (req, res) => {
-  //   const tickets = {
-  //     id: req.user._id,
-  //     email: req.user.email,
-  //     name: req.user.name,
-  //   };
-  //   res.status(200).json(tickets);
-  res.status(200).json({ message: 'create new ticket' });
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error('Please include a product and description');
+  }
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('You must login to create a ticket');
+  }
+
+  const ticket = await Ticket.create({
+    user: req.user.id,
+    product,
+    description,
+    status: 'new',
+  });
+
+  res.status(201).json(ticket);
 });
 
 module.exports = { getTickets, createTicket };
