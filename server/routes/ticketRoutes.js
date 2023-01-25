7bd7aa2d27322fcf2d91bb1da4a049bf704b896b
I-Maged <magedhanafy@gmail.com>
2023-01-25 11:35:08 +0200

    note routes

--- server/routes/ticketRoutes.js
+++ server/routes/ticketRoutes.js
@@ -11,6 +11,9 @@ const {
   updateTicket,
 } = require('../controllers/ticketController');
 
+const noteRouter = require('./noteRoutes');
+router.use('/:ticketId/notes', noteRouter);
+
 router.route('/').get(protect, getTickets).post(protect, createTicket);
 
 router
